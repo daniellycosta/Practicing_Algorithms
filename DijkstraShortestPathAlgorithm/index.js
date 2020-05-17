1434edcb33dbd7d49896f0045854fc88bfef8e9a
Danielly Costa <daniellycmcosta@gmail.com>
2020-05-17 18:39:12 -0300

    fix console.log

--- DijkstraShortestPathAlgorithm/index.js
+++ DijkstraShortestPathAlgorithm/index.js
@@ -61,7 +61,7 @@ const DijkstraFactory = (graph, startVertice) => {
         );
 
         console.log(
-          `Already visited: ${[0, 1, 2, 3, 4, 5, 6, 7, 8].filter((item) => {
+          `Already visited: ${[...Array(graph.length).keys()].filter((item) => {
             return !!sptSet[item];
           })}`
         );
