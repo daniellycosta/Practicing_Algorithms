29307c0f6016f09bd2189ae274f8440df45159a3
Danielly Costa <daniellycmcosta@gmail.com>
2020-05-17 17:55:04 -0300

    changing to factory pattern

--- DijkstraShortestPathAlgorithm/index.js
+++ DijkstraShortestPathAlgorithm/index.js
@@ -1,11 +1,107 @@
-let sptSet = [false, false, false, false, false, false, false, false, false];
-const graph = [
+const DijkstraFactory = (graph, startVertice) => {
+  let sptSet = new Array(graph.length).fill(false);
+
+  setSptSet = (i) => {
+    sptSet[i] = true;
+  };
+
+  getNextVertice = (currentVertice) => {
+    const availableNeighbors = graph[currentVertice].neighbors.filter(
+      (neighbor) => !sptSet[neighbor]
+    );
+
+    console.log(
+      `${
+        availableNeighbors.length ? availableNeighbors : "none"
+      } available from  ${graph[currentVertice].neighbors}`
+    );
+
+    return availableNeighbors.length
+      ? graph[currentVertice].neighbors.reduce(
+          (lowerDistanceVerticeI, neighbor) => {
+            if (!sptSet[neighbor]) {
+              return graph[neighbor].distanceValue <
+                graph[lowerDistanceVerticeI].distanceValue
+                ? neighbor
+                : lowerDistanceVerticeI;
+            }
+            return lowerDistanceVerticeI;
+          },
+          availableNeighbors[0]
+        )
+      : -1;
+  };
+
+  printResult = () => {
+    console.log(
+      graph.map(({ distanceValue, previousVertice }, i) => ({
+        vertice: i,
+        distanceValue,
+        previousVertice,
+      }))
+    );
+  };
+
+  return {
+    run: () => {
+      console.log("Starting Dijkstra's Algorithm...");
+      let currentVertice = startVertice;
+      graph[startVertice].distanceValue = 0;
+
+      while (
+        sptSet.filter((vertice) => vertice).length !== graph.length &&
+        currentVertice !== -1
+      ) {
+        setSptSet(currentVertice);
+
+        console.log(
+          `\n\nRunning vertice ${currentVertice}, visited ${
+            sptSet.filter((vertice) => vertice).length
+          } vertices of ${graph.length}`
+        );
+
+        console.log(
+          `Already visited: ${[0, 1, 2, 3, 4, 5, 6, 7, 8].filter((item) => {
+            return !!sptSet[item];
+          })}`
+        );
+
+        graph[currentVertice].neighbors.map((neighbor, i) => {
+          if (!sptSet[neighbor]) {
+            const newDistance =
+              graph[currentVertice].edgeValue[i] +
+              graph[currentVertice].distanceValue;
+
+            console.log(
+              `New Distance from ${currentVertice} to ${neighbor}: ${graph[currentVertice].edgeValue[i]} + ${graph[currentVertice].distanceValue} `
+            );
+
+            if (newDistance < graph[neighbor].distanceValue) {
+              graph[neighbor].distanceValue = newDistance;
+              graph[neighbor].previousVertice = currentVertice;
+            }
+
+            console.log(
+              `Distance from ${currentVertice} to ${neighbor}: ${graph[neighbor].distanceValue}`
+            );
+          }
+        });
+        console.log("Calculating next vertice...");
+        currentVertice = getNextVertice(currentVertice);
+      }
+      console.log("\n\nPrinting result...\n\n");
+      printResult();
+    },
+  };
+};
+
+const exampleGraph = [
   {
     //0
     neighbors: [1, 7],
     edgeValue: [4, 8],
-    distanceValue: 0,
-    previousVertice: 0,
+    distanceValue: Infinity,
+    previousVertice: null,
   },
   {
     //1
@@ -65,87 +161,5 @@ const graph = [
   },
 ];
 
-const setSptSet = (i) => {
-  sptSet[i] = true;
-};
-
-const getNextVertice = (currentVertice) => {
-  const availableNeighbors = graph[currentVertice].neighbors.filter(
-    (neighbor) => !sptSet[neighbor]
-  );
-
-  console.log(
-    `${
-      availableNeighbors.length ? availableNeighbors : "none"
-    } available from  ${graph[currentVertice].neighbors}`
-  );
-
-  return availableNeighbors.length
-    ? graph[currentVertice].neighbors.reduce(
-        (lowerDistanceVerticeI, neighbor) => {
-          if (!sptSet[neighbor]) {
-            return graph[neighbor].distanceValue <
-              graph[lowerDistanceVerticeI].distanceValue
-              ? neighbor
-              : lowerDistanceVerticeI;
-          }
-          return lowerDistanceVerticeI;
-        },
-        availableNeighbors[0]
-      )
-    : -1;
-};
-
-const runDijktras = () => {
-  console.log("Starting Dijkstra's Algorithm...");
-  let currentVertice = 0;
-  while (
-    sptSet.filter((vertice) => vertice).length !== graph.length &&
-    currentVertice !== -1
-  ) {
-    setSptSet(currentVertice);
-
-    console.log(
-      `\n\nRunning vertice ${currentVertice}, visited ${
-        sptSet.filter((vertice) => vertice).length
-      } vertices of ${graph.length}`
-    );
-
-    console.log(
-      `Already visited: ${[0, 1, 2, 3, 4, 5, 6, 7, 8].filter((item) => {
-        return !!sptSet[item];
-      })}`
-    );
-
-    graph[currentVertice].neighbors.map((neighbor, i) => {
-      if (!sptSet[neighbor]) {
-        const newDistance =
-          graph[currentVertice].edgeValue[i] +
-          graph[currentVertice].distanceValue;
-
-        console.log(
-          `New Distance from ${currentVertice} to ${neighbor}: ${graph[currentVertice].edgeValue[i]} + ${graph[currentVertice].distanceValue} `
-        );
-
-        if (newDistance < graph[neighbor].distanceValue) {
-          graph[neighbor].distanceValue = newDistance;
-          graph[neighbor].previousVertice = currentVertice;
-        }
-
-        console.log(
-          `Distance from ${currentVertice} to ${neighbor}: ${graph[neighbor].distanceValue}`
-        );
-      }
-    });
-    console.log("Calculating next vertice...");
-    currentVertice = getNextVertice(currentVertice);
-  }
-  console.log("\n\nFiltering result...\n\n");
-  return graph.map(({ distanceValue, previousVertice }, i) => ({
-    vertice: i,
-    distanceValue,
-    previousVertice,
-  }));
-};
-
-console.log(runDijktras());
+const Test = DijkstraFactory(exampleGraph, 0);
+Test.run();
